a0e72083615e4a1a0dd4ecba88d531438d71b66e
Sandra Müller <muelsand20@gmail.com>
2022-06-26 23:21:49 +0200

    header and about-me finished, slider for projects implemented

---
+++ src/components/Slider.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import './slider.css';
+import projectsArray from './projectsArray';
+
+const Slider = () => {
+    const handleSlider = (e) => {
+        const buttonsWrapper = document.querySelector(".map");
+        const slides = document.querySelector(".inner");
+        if (e.target.nodeName === "BUTTON") {
+            Array.from(buttonsWrapper.children).forEach(item =>
+              item.classList.remove("active")
+            );
+            if (e.target.classList.contains("first")) {
+              slides.style.transform = "translateX(-0%)";
+              e.target.classList.add("active");
+            } else if (e.target.classList.contains("second")) {
+              slides.style.transform = "translateX(-33.33333333333333%)";
+              e.target.classList.add("active");
+            } else if (e.target.classList.contains('third')){
+              slides.style.transform = 'translatex(-66.6666666667%)';
+              e.target.classList.add('active');
+            }
+        }
+    }
+
+    return(
+        <div className="center">
+            <div className="wrapper">
+                <div className="inner">
+                    {
+                        projectsArray.map(slide => {
+                            return(
+                            <div className="card" key={slide.id} >
+                                <img src={slide.src} />
+                                <div className="content">
+                                    <h1>{slide.headline}</h1>
+                                    <h3>{slide.description}</h3>
+                                </div>
+                            </div> 
+                            );
+                        })
+                    }
+                </div>
+            </div>
+            
+            <div className="map" onClick={handleSlider} >
+                <button className="active first"></button>
+                <button className="second"></button>
+                <button className="third"></button>
+            </div>
+        </div>
+    );
+}
+
+export default Slider;
