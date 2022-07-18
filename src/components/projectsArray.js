3e96a34ad651135abc003547b0dcff4230dfa23a
Sandra Müller <muelsand20@gmail.com>
2022-07-18 20:45:53 +0200

    second project added

--- src/components/projectsArray.js
+++ src/components/projectsArray.js
@@ -1,5 +1,7 @@
 import MemoryGameGif from './../projectMedia/MemoryDemo.gif';
 import MemoryGameThumbnail from './../projectMedia/MemoryDemo.png';
+import PopupGameGif from './../projectMedia/PopupGame.gif';
+import PopupGameThumbnail from './../projectMedia/PopupGame.png';
 
 const projectsArray = [
     {
@@ -11,9 +13,10 @@ const projectsArray = [
     },
     {
         "id": 2,
-        "src": "",
-        "headline": "My Project",
-        "description": "Coming Soon"
+        "imgSrc": PopupGameThumbnail,
+        "gifSrc": PopupGameGif,
+        "headline": "Popup Game",
+        "description": "I did this project in an amazing group of four people. We only had 8 hours to find the idea and complete the application. (18. July)"
     },
     {
         "id": 3,
