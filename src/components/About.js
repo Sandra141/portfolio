870c70ec47508f173e2c500a583bba073fef0282
Sandra Müller <muelsand20@gmail.com>
2022-07-19 17:06:53 +0200

    about me section modified

--- src/components/About.js
+++ src/components/About.js
@@ -14,11 +14,13 @@ const Contact = () => {
                 <p id='about'>------ Who I am ------</p>
                 <h2>About Me</h2>
                 <p>
-                    I am Sandra Müller. I dropped out of my interesting and challenging studies at the Ludwigshafen University of Business and Society because I was able to gain many insights into the diverse Japanese culture there. Since then, I have fallen in love with programming. <br /><br />
-                    So why web development? I started working with HTML and CSS while I was still at school. At the time, I was writing my seminar paper and had trouble organising my sources, so I created a simple but structured website. <br /><br />
+                    Hi, I am Sandra Müller. I studied at the University of Business and Society in Ludwigshafen for four interesting and challenging semesters and was able to gain many insights into the diverse Japanese culture. Since then, I have fallen in love with programming. <br /><br />
+                    But why web development? I started working with HTML and CSS while I was still at school. At the time, I was writing my seminar paper and had trouble organising my sources, so I created a simple but structured website. <br /><br />
                     The final trigger for my passion, however, was my working student job in a small company, where I got to know the work of a web developer, which is why I took part in a bootcamp shortly afterwards and why programming now won't let me go.
                 </p>
-                <button>Download CV</button>
+                <a href="#contact">
+                    <button>Contact Me</button> {/* For later: Download CV */}
+                </a>
             </div>
         </div>
     );
